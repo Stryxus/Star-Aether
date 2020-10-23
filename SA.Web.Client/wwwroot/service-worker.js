75f5548b78fc9cdcf297e496599be53b6e8fd0d0
Connor Shearer <connor_shearer@outlook.com>
2020-10-23 17:06:59 +0100

    Update service worker

--- SA.Web.Client/wwwroot/service-worker.js
+++ SA.Web.Client/wwwroot/service-worker.js
@@ -5,42 +5,36 @@ self.addEventListener('fetch', event => event.respondWith(onFetch(event)));
 
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
-const offlineAssetsInclude = [/\.dll$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.txt$/, /\.css$/, /\.woff2$/, /\.svg$/, /\.webm$/, /\.webp$/ ];
+const offlineAssetsInclude = [ /\.dll$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.webp$/, /\.gif$/, /\.blat$/, /\.dat$/ ];
 const offlineAssetsExclude = [ /^service-worker\.js$/ ];
 
-async function onInstall(event) 
-{
+async function onInstall(event) {
+    console.info('Star Athenaeum Service Worker: Installing...');
     const assetsRequests = self.assetsManifest.assets
-    .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
-    .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-    .map(asset => new Request(asset.url, { integrity: asset.hash }));
+        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => new Request(asset.url, { integrity: asset.hash }));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
+    console.info('Star Athenaeum Service Worker: Successfully installed!');
 }
 
-async function onActivate(event) 
-{
+async function onActivate(event) {
+    console.info('Star Athenaeum Service Worker: Activating...');
+
     const cacheKeys = await caches.keys();
     await Promise.all(cacheKeys
         .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
         .map(key => caches.delete(key)));
+        console.info('Star Athenaeum Service Worker: Successfully activated!');
 }
 
-async function onFetch(event) 
-{
-    try 
-    {
-        let cachedResponse = null;
-        if (event.request.method === 'GET') 
-        {
-            const shouldServeIndexHtml = event.request.mode === 'navigate';
-            const request = shouldServeIndexHtml ? 'index.html' : event.request;
-            const cache = await caches.open(cacheName);
-            cachedResponse = await cache.match(request);
-        }
-        return cachedResponse || fetch(event.request);
-    } 
-    catch (error) 
-    {
-        return fetch(event.request);
+async function onFetch(event) {
+    let cachedResponse = null;
+    if (event.request.method === 'GET') {
+        const shouldServeIndexHtml = event.request.mode === 'navigate';
+        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+        const cache = await caches.open(cacheName);
+        cachedResponse = await cache.match(request);
     }
+    return cachedResponse || fetch(event.request);
 }
