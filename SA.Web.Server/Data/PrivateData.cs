d271520f9dc4f3cd5592af2397b0be15fc384b67
Connor Shearer <connor_shearer@outlook.com>
2021-04-17 07:59:45 +0100

    Add class to read private data

---
+++ SA.Web.Server/Data/PrivateData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+using Newtonsoft.Json;
+
+namespace SA.Web.Server.Data
+{
+    public class PrivateVariables
+    {
+        public string MongoDBConnectionString { get; set; }
+
+        //
+
+        internal static PrivateVariables _instance;
+        internal static PrivateVariables Instance
+        {
+            get { return _instance != null ? _instance : _instance = JsonConvert.DeserializeObject<PrivateVariables>(File.ReadAllText("private.json")); }
+            private set { _instance = value; }
+        }
+
+        private static void GetJson() => Instance = JsonConvert.DeserializeObject<PrivateVariables>(File.ReadAllText("private.json"));
+    }
+}
