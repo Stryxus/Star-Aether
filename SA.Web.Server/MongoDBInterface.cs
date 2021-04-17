5cc37f86f4f4fec4edeb630d105237fac88f093a
Connor Shearer <connor_shearer@outlook.com>
2021-04-17 09:55:45 +0100

    Exception handle data retrieval from database

--- SA.Web.Server/MongoDBInterface.cs
+++ SA.Web.Server/MongoDBInterface.cs
@@ -27,7 +27,7 @@ namespace SA.Web.Server
             if (Client == null)
             {
                 await Logger.LogInfo("Connecting to MongoDB...");
-                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(""));
+                MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(PrivateData.Instance.MongoDBConnectionString));
                 settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls13 };
                 Client = new MongoClient(settings);
                 Database_Public_Data = Client.GetDatabase("public_data");
@@ -46,28 +46,70 @@ namespace SA.Web.Server
         internal async Task<string> GetNewsData()
         {
             NewsData data = new NewsData { NewsPosts = new List<NewsEntryData>() };
-            //foreach (BsonDocument entry in await (await Database_Public_Data.GetCollection<BsonDocument>("news-data").FindAsync(_ => true)).ToListAsync()) data.NewsPosts.Add(BsonSerializer.Deserialize<NewsEntryData>(entry));
+            try
+            {
+                foreach (BsonDocument entry in await (await Database_Public_Data.GetCollection<BsonDocument>("news-data").FindAsync(_ => true)).ToListAsync()) data.NewsPosts.Add(BsonSerializer.Deserialize<NewsEntryData>(entry));
+            } catch (TimeoutException)
+            {
+                await Logger.LogWarn("News data request timed out - Using default data structure.");
+            } catch (NullReferenceException)
+            {
+                await Logger.LogError("News data request threw null - Using default data structure.");
+            }
             return JsonConvert.SerializeObject(data);
         }
 
         internal async Task<string> GetRoadmapData()
         {
             RoadmapData data = new RoadmapData { Cards = new List<RoadmapCardData>() };
-            //foreach (BsonDocument entry in await (await Database_Public_Data.GetCollection<BsonDocument>("roadmap-data").FindAsync(_ => true)).ToListAsync()) data.Cards.Add(BsonSerializer.Deserialize<RoadmapCardData>(entry));
+            try
+            {
+                foreach (BsonDocument entry in await (await Database_Public_Data.GetCollection<BsonDocument>("roadmap-data").FindAsync(_ => true)).ToListAsync()) data.Cards.Add(BsonSerializer.Deserialize<RoadmapCardData>(entry));
+            }
+            catch (TimeoutException)
+            {
+                await Logger.LogWarn("News data request timed out - Using default data structure.");
+            }
+            catch (NullReferenceException)
+            {
+                await Logger.LogError("News data request threw null - Using default data structure.");
+            }
             return JsonConvert.SerializeObject(data);
         }
 
         internal async Task<string> GetChangelogData()
         {
             ChangelogData data = new ChangelogData { ChangelogPosts = new List<ChangelogEntryData>() };
-            //foreach (BsonDocument entry in await (await Database_Public_Data.GetCollection<BsonDocument>("changelog-data").FindAsync(_ => true)).ToListAsync()) data.ChangelogPosts.Add(BsonSerializer.Deserialize<ChangelogEntryData>(entry));
+            try
+            {
+                foreach (BsonDocument entry in await (await Database_Public_Data.GetCollection<BsonDocument>("changelog-data").FindAsync(_ => true)).ToListAsync()) data.ChangelogPosts.Add(BsonSerializer.Deserialize<ChangelogEntryData>(entry));
+            }
+            catch (TimeoutException)
+            {
+                await Logger.LogWarn("News data request timed out - Using default data structure.");
+            }
+            catch (NullReferenceException)
+            {
+                await Logger.LogError("News data request threw null - Using default data structure.");
+            }
             return JsonConvert.SerializeObject(data);
         }
 
         internal async Task<string> GetPhotographyData()
         {
             MediaPhotographyData data = new MediaPhotographyData { Photos = new List<MediaPhoto>() };
-            //foreach (BsonDocument entry in await (await Database_Public_Data.GetCollection<BsonDocument>("photography-data").FindAsync(_ => true)).ToListAsync()) data.Photos.Add(BsonSerializer.Deserialize<MediaPhoto>(entry));
+            try
+            {
+                foreach (BsonDocument entry in await (await Database_Public_Data.GetCollection<BsonDocument>("photography-data").FindAsync(_ => true)).ToListAsync()) data.Photos.Add(BsonSerializer.Deserialize<MediaPhoto>(entry));
+            }
+            catch (TimeoutException)
+            {
+                await Logger.LogWarn("News data request timed out - Using default data structure.");
+            }
+            catch (NullReferenceException)
+            {
+                await Logger.LogError("News data request threw null - Using default data structure.");
+            }
             return JsonConvert.SerializeObject(data);
         }
     }
