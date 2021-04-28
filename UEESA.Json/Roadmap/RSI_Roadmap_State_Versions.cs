16cb5f0fb0612f5cfa4fb9154ade20ce113e381a
Connor Shearer <connor_shearer@outlook.com>
2021-04-28 12:33:35 +0100

    Add UEESA.Json project to unify and centralise all Json data

---
+++ UEESA.Json/Roadmap/RSI_Roadmap_State_Versions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UEESA.Json.Roadmap
+{
+    class RSI_Roadmap_State_Versions
+    {
+    }
+}
