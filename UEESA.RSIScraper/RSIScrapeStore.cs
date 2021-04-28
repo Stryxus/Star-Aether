7f0768f48004ec79344177b1bb95ef99b4ce4c83
Connor Shearer <connor_shearer@outlook.com>
2021-04-28 15:24:34 +0100

    Add roadmap state to store

--- UEESA.RSIScraper/RSIScrapeStore.cs
+++ UEESA.RSIScraper/RSIScrapeStore.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 
+using UEESA.Json.Roadmap;
+
 namespace UEESA.RSIScraper
 {
     internal static class RSIScrapeStore
     {
-
+        public static RSI_Roadmap_State RoadmapState { internal set; get; }
     }
 }
