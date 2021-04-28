3037be8437260dccf0d9a1051d972ef2c8b2dd76
Connor Shearer <connor_shearer@outlook.com>
2021-04-28 13:02:10 +0100

    Add RSI URL's

--- UEESA.RSIScraper/RSIScraperCore.cs
+++ UEESA.RSIScraper/RSIScraperCore.cs
@@ -8,6 +8,11 @@ namespace UEESA.RSIScraper
 {
     internal static class RSIScraperCore
     {
-
+        internal static Uri URL_RSI = new Uri("https://robertsspaceindustries.com/");
+        internal static Uri URL_RSI_Transmissions = new Uri("https://robertsspaceindustries.com/comm-link");
+        internal static Uri URL_RSI_Spectrum = new Uri("https://robertsspaceindustries.com/spectrum/community/SC");
+        internal static Uri URL_RSI_Roadmap = new Uri("https://robertsspaceindustries.com/roadmap/release-view");
+        internal static Uri URL_RSI_Telemetry = new Uri("https://robertsspaceindustries.com/telemetry");
+        internal static Uri URL_RSI_IssueCouncil = new Uri("https://issue-council.robertsspaceindustries.com/");
     }
 }
