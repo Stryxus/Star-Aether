f2ed516dc6ebdfe3a9fb86dfee34bfaf0209f566
Connor Shearer <connor_shearer@outlook.com>
2021-05-23 22:26:57 +0100

    Specify the animation time from .NET and clean up some delays

--- Core/UEESA.Client/Data/JSInterface.cs
+++ Core/UEESA.Client/Data/JSInterface.cs
@@ -39,14 +39,21 @@ namespace UEESA.Client.Data
 
         public class AnimationManager
         {
-            public async Task FadePageOut() => await JSR.InvokeVoidAsync("animationInterface.fadeOutPage");
-            public async Task FadePageIn(bool bypass) => await JSR.InvokeVoidAsync("animationInterface.fadeInPage", bypass);
-            public async Task FadeInBackground() => await JSR.InvokeVoidAsync("animationInterface.fadeInBackground");
-            public async Task SlideInNavigationBar() => await JSR.InvokeVoidAsync("animationInterface.slideInNavigationBar");
-            public async Task SlideInToolsBar() => await JSR.InvokeVoidAsync("animationInterface.slideInToolsBar");
-            public async Task SlideInOutHeadlinesNavBarTicker(bool slideIn) => await JSR.InvokeVoidAsync("animationInterface.slideInOutHeadlinesNavBarTicker", slideIn);
-            public async Task SlideInOutEonomeNavBarTicker(bool slideIn) => await JSR.InvokeVoidAsync("animationInterface.slideInOutEonomeNavBarTicker", slideIn);
-            public async Task SlideInOutSettingsPanel(bool slideIn) => await JSR.InvokeVoidAsync("animationInterface.slideInOutSettingsPanel", slideIn);
+            public float Time_PageFade = 0.3F;
+            public float Time_BackgroundFade = 1F;
+            public float Time_NavigationBarSlideIn = 1F;
+            public float Time_ToolsBarSlideIn = 1F;
+            public float Time_NavigationBarTickerSlide = 1.5F;
+            public float Time_SettingsPanelSlide = 0.5F;
+
+            public async Task FadePageOut() => await JSR.InvokeVoidAsync("animationInterface.fadeOutPage", Time_PageFade);
+            public async Task FadePageIn(bool bypass) => await JSR.InvokeVoidAsync("animationInterface.fadeInPage", Time_PageFade, bypass);
+            public async Task FadeInBackground() => await JSR.InvokeVoidAsync("animationInterface.fadeInBackground", Time_BackgroundFade);
+            public async Task SlideInNavigationBar() => await JSR.InvokeVoidAsync("animationInterface.slideInNavigationBar", Time_NavigationBarSlideIn);
+            public async Task SlideInToolsBar() => await JSR.InvokeVoidAsync("animationInterface.slideInToolsBar", Time_ToolsBarSlideIn);
+            public async Task SlideInOutHeadlinesNavBarTicker(bool slideIn) => await JSR.InvokeVoidAsync("animationInterface.slideInOutHeadlinesNavBarTicker", Time_NavigationBarTickerSlide, slideIn);
+            public async Task SlideInOutEonomeNavBarTicker(bool slideIn) => await JSR.InvokeVoidAsync("animationInterface.slideInOutEonomeNavBarTicker", Time_NavigationBarTickerSlide, slideIn);
+            public async Task SlideInOutSettingsPanel(bool slideIn) => await JSR.InvokeVoidAsync("animationInterface.slideInOutSettingsPanel", Time_SettingsPanelSlide, slideIn);
         }
     }
 }
--- Source_Assets/js/animation-manager.js
+++ Source_Assets/js/animation-manager.js
@@ -11,74 +11,74 @@ GLOBAL.animationInterface = function (ref)
 
     window.animationInterface = {
 
-        fadeOutPage: () => 
+        fadeOutPage: (timeSecs) => 
         {
-            TweenMax.to("#webpage", 0.3, { opacity: 0, ease: "power3.out" });
+            TweenMax.to("#webpage", timeSecs, { opacity: 0, ease: "power3.out" });
         },
-        fadeInPage: (bypass) => 
+        fadeInPage: (timeSecs, bypass) => 
         {
             if (!bypass) 
             {
-                TweenMax.to("#webpage", 0.3, { opacity: 1, ease: "power3.out" });
+                TweenMax.to("#webpage", timeSecs, { opacity: 1, ease: "power3.out" });
             }
             else 
             {
                 document.getElementById("webpage").style.opacity = 1;
             }
         },
-        fadeInBackground: () => 
+        fadeInBackground: (timeSecs) => 
         {
-            TweenMax.to("#background", 1, { opacity: 1, ease: "power3.out" });
+            TweenMax.to("#background", timeSecs, { opacity: 1, ease: "power3.out" });
         },
-        slideInNavigationBar: () => 
+        slideInNavigationBar: (timeSecs) => 
         {
             var height = document.getElementById("main-nav").offsetHeight;
             document.getElementById("main-nav").style.bottom = -height;
-            TweenMax.to("#main-nav", 1, { bottom: 0, ease: "power3.out" });
+            TweenMax.to("#main-nav", timeSecs, { bottom: 0, ease: "power3.out" });
         },
-        slideInToolsBar: () => 
+        slideInToolsBar: (timeSecs) => 
         {
             var width = document.getElementById("tools-sidebar").offsetWidth;
             document.getElementById("tools-sidebar").style.left = -width;
-            TweenMax.to("#tools-sidebar", 1, { left: 0, ease: "power3.out" });
+            TweenMax.to("#tools-sidebar", timeSecs, { left: 0, ease: "power3.out" });
         },
-        slideInOutHeadlinesNavBarTicker: (slideIn) => 
+        slideInOutHeadlinesNavBarTicker: (timeSecs, slideIn) => 
         {
             var width = document.getElementById("headlines-navbar-ticker").offsetWidth;
             if (!slideIn) 
             {
                 document.getElementById("headlines-navbar-ticker").style.right = -width;
-                TweenMax.to("#headlines-navbar-ticker", 1.5, { right: 0, ease: "power3.out" });
+                TweenMax.to("#headlines-navbar-ticker", timeSecs, { right: 0, ease: "power3.out" });
             }
             else 
             {
-                TweenMax.to("#headlines-navbar-ticker", 1.5, { right: -width, ease: "power3.out" });
+                TweenMax.to("#headlines-navbar-ticker", timeSecs, { right: -width, ease: "power3.out" });
             }
         },
-        slideInOutEonomeNavBarTicker: (slideIn) => 
+        slideInOutEonomeNavBarTicker: (timeSecs, slideIn) => 
         {
             var width = document.getElementById("econome-navbar-ticker").offsetWidth;
             if (!slideIn) 
             {
                 document.getElementById("econome-navbar-ticker").style.right = -width;
-                TweenMax.to("#econome-navbar-ticker", 1.5, { right: 0, ease: "power3.out" });
+                TweenMax.to("#econome-navbar-ticker", timeSecs, { right: 0, ease: "power3.out" });
             }
             else 
             {
-                TweenMax.to("#econome-navbar-ticker", 1.5, { right: -width, ease: "power3.out" });
+                TweenMax.to("#econome-navbar-ticker", timeSecs, { right: -width, ease: "power3.out" });
             }
         },
-        slideInOutSettingsPanel: (slideIn) => 
+        slideInOutSettingsPanel: (timeSecs, slideIn) => 
         {
             var width = document.getElementById("settings-panel").offsetWidth;
             if (!slideIn) 
             {
                 document.getElementById("settings-panel").style.right = -width;
-                TweenMax.to("#settings-panel", 0.5, { right: 0, ease: "power3.out" });
+                TweenMax.to("#settings-panel", timeSecs, { right: 0, ease: "power3.out" });
             }
             else 
             {
-                TweenMax.to("#settings-panel", 0.5, { right: -width, ease: "power3.out" });
+                TweenMax.to("#settings-panel", timeSecs, { right: -width, ease: "power3.out" });
             }
         }
     }
