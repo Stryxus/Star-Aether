dbafcbfbbc299d186df7238fc56cbd745a6b71d6
Connor Shearer <connor_shearer@outlook.com>
2021-05-23 05:03:31 +0100

    Remove WebGL2 code for now

--- Source_Assets/js/runtime.js
+++ Source_Assets/js/runtime.js
@@ -59,22 +59,7 @@ GLOBAL.runtime = function (ref)
         },
         loadWebGL2: () => 
         {
-            var canv = document.getElementById("stargazer-canvas");
-            var gl = canv.getContext("webgl2");
-            if (gl) {
-                if (!webglalerted) {
-                    webglalerted = true;
-                    alert("This is a WebGL2 testing placeholder. If you see a black screen, its_working.gif!");
-                }
-
-                gl.viewportWidth = canv.width;
-                gl.viewportHeight = canv.height;
-                window.addEventListener("resize", () => {
-                    resize(gl, true);
-                });
-                gl.clearColor(0.0, 0.0, 0.0, 1.0);
-                gl.clear(gl.COLOR_BUFFER_BIT);
-            }
+            
         }
     }
 
