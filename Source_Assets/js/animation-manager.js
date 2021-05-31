ce459ca505a28eeeaebf8d393175ebe01aa2005e
Connor Shearer <connor_shearer@outlook.com>
2021-05-31 15:17:20 +0100

    Add element existence checks for the animation JS

--- Source_Assets/js/animation-manager.js
+++ Source_Assets/js/animation-manager.js
@@ -9,61 +9,80 @@ GLOBAL.animationInterface = function (ref)
     window.animationInterface = {
         fadeInOutPage: (timeSecs, fadeIn) => 
         {
-            if (!fadeIn) TweenMax.to("#page-body", timeSecs, { opacity: 0, yPercent: -5, ease: "power3.out" });
-            else 
+            if (document.getElementById("page-body") !== null) 
             {
-                TweenMax.to("#page-body", 0, { yPercent: 5 });
-                TweenMax.to("#page-body", timeSecs, { opacity: 1, yPercent: 0, ease: "power3.out" });
+                if (!fadeIn) TweenMax.to("#page-body", timeSecs, { opacity: 0, yPercent: -5, ease: "power3.out" });
+                else 
+                {
+                    TweenMax.to("#page-body", 0, { yPercent: 5 });
+                    TweenMax.to("#page-body", timeSecs, { opacity: 1, yPercent: 0, ease: "power3.out" });
+                }
             }
         },
         fadeInOutBackground: (timeSecs, fadeIn) => 
         {
-            if (document.getElementById("background") !== null) {
+            if (document.getElementById("background") !== null) 
+            {
                 if (!fadeIn) TweenMax.to("#background", timeSecs, { opacity: 0, ease: "power3.out" });
                 else TweenMax.to("#background", timeSecs, { opacity: 1, ease: "power3.out" });
             }
         },
         slideInNavigationBar: (timeSecs) => 
         {
-            var height = document.getElementById("main-nav").offsetHeight;
-            TweenMax.to("#main-nav", 0, { bottom: -height });
-            TweenMax.to("#main-nav", timeSecs, { bottom: 0, ease: "power3.out" });
+            if (document.getElementById("main-nav") !== null)
+            {
+                var height = document.getElementById("main-nav").offsetHeight;
+                TweenMax.to("#main-nav", 0, { bottom: -height });
+                TweenMax.to("#main-nav", timeSecs, { bottom: 0, ease: "power3.out" });
+            }
         },
         slideInToolsBar: (timeSecs) => 
         {
-            var width = document.getElementById("tools-sidebar").offsetWidth;
-            TweenMax.to("#tools-sidebar", 0, { left: -width });
-            TweenMax.to("#tools-sidebar", timeSecs, { left: 0, ease: "power3.out" });
+            if (document.getElementById("tools-sidebar") !== null)
+            {
+                var width = document.getElementById("tools-sidebar").offsetWidth;
+                TweenMax.to("#tools-sidebar", 0, { left: -width });
+                TweenMax.to("#tools-sidebar", timeSecs, { left: 0, ease: "power3.out" });
+            }
         },
         slideInOutHeadlinesNavBarTicker: (timeSecs, slideOut) => 
         {
-            var width = document.getElementById("headlines-navbar-ticker").offsetWidth;
-            if (slideOut) 
+            if (document.getElementById("headlines-navbar-ticker") !== null)
             {
-                TweenMax.to("#headlines-navbar-ticker", 0, { right: -width });
-                TweenMax.to("#headlines-navbar-ticker", timeSecs, { right: 0, ease: "power3.out" });
+                var width = document.getElementById("headlines-navbar-ticker").offsetWidth;
+                if (slideOut) 
+                {
+                    TweenMax.to("#headlines-navbar-ticker", 0, { right: -width });
+                    TweenMax.to("#headlines-navbar-ticker", timeSecs, { right: 0, ease: "power3.out" });
+                }
+                else TweenMax.to("#headlines-navbar-ticker", timeSecs, { right: -width, ease: "power3.out" });
             }
-            else TweenMax.to("#headlines-navbar-ticker", timeSecs, { right: -width, ease: "power3.out" });
         },
         slideInOutEonomeNavBarTicker: (timeSecs, slideOut) => 
         {
-            var width = document.getElementById("econome-navbar-ticker").offsetWidth;
-            if (slideOut) 
+            if (document.getElementById("econome-navbar-ticker") !== null) 
             {
-                TweenMax.to("#econome-navbar-ticker", 0, { right: -width });
-                TweenMax.to("#econome-navbar-ticker", timeSecs, { right: 0, ease: "power3.out" });
+                var width = document.getElementById("econome-navbar-ticker").offsetWidth;
+                if (slideOut) 
+                {
+                    TweenMax.to("#econome-navbar-ticker", 0, { right: -width });
+                    TweenMax.to("#econome-navbar-ticker", timeSecs, { right: 0, ease: "power3.out" });
+                }
+                else TweenMax.to("#econome-navbar-ticker", timeSecs, { right: -width, ease: "power3.out" });
             }
-            else TweenMax.to("#econome-navbar-ticker", timeSecs, { right: -width, ease: "power3.out" });
         },
         slideInOutSettingsPanel: (timeSecs, slideOut) => 
         {
-            var width = document.getElementById("settings-panel").offsetWidth;
-            if (slideOut) 
+            if (document.getElementById("settings-panel") !== null) 
             {
-                TweenMax.to("#settings-panel", 0, { right: -width });
-                TweenMax.to("#settings-panel", timeSecs, { right: 0, ease: "power3.out" });
+                var width = document.getElementById("settings-panel").offsetWidth;
+                if (slideOut) 
+                {
+                    TweenMax.to("#settings-panel", 0, { right: -width });
+                    TweenMax.to("#settings-panel", timeSecs, { right: 0, ease: "power3.out" });
+                }
+                else TweenMax.to("#settings-panel", timeSecs, { right: -width, ease: "power3.out" });
             }
-            else TweenMax.to("#settings-panel", timeSecs, { right: -width, ease: "power3.out" });
         }
     }
 })();
