a8e061744cfe87d3577228ac5789983d04316ab1
Connor Shearer <connor_shearer@outlook.com>
2021-08-10 04:04:33 +0100

    Create the confidential socket handlers

---
+++ Core/UEESA.Client/Sockets/Handlers/ConfidentialSocketHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.WebSockets;
+
+using UEESA.Shared.Sockets;
+
+namespace UEESA.Client.Sockets.Handlers
+{
+    public class ConfidentialSocketHandler : WebSocketHandler<WebSocket>
+    {
+        public ConfidentialSocketHandler(ConnectionManager<WebSocket> webSocketConnectionManager) : base(webSocketConnectionManager) { }
+
+        public override void OnConnected(WebSocket socket) => base.OnConnected(socket);
+
+        public override void Receive(WebSocket socket, WebSocketReceiveResult result, string message)
+        {
+
+        }
+    }
+}
