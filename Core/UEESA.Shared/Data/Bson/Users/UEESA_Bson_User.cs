51145f8f5366717802eb267e1ee0819da9a0348d
Connor Shearer <connor_shearer@outlook.com>
2021-08-10 04:04:52 +0100

    Define the first user parameters

--- Core/UEESA.Shared/Data/Bson/Users/UEESA_Bson_User.cs
+++ Core/UEESA.Shared/Data/Bson/Users/UEESA_Bson_User.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Security;
 
 namespace UEESA.Shared.Data.Bson.Users
 {
     public class UEESA_Bson_User
     {
+        public string User_Username { get; set; }
+        public SecureString User_Hash_Email { get; set; }
+        public SecureString User_Hash_Password { get; set; }
     }
 }
