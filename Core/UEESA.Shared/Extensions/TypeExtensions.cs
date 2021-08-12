4652a42adef1a0a3175d29bbaa40d02f0255c36f
Connor Shearer <connor_shearer@outlook.com>
2021-08-12 08:43:01 +0100

    Create TypeExtensions.cs

---
+++ Core/UEESA.Shared/Extensions/TypeExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace UEESA.Shared.Extensions
+{
+    public static class TypeExtensions
+    {
+		public static bool HasMethod(this Type type, string methodnName)
+		{
+			if (type == null)
+			{
+				throw new ArgumentNullException("type");
+			}
+			if (methodnName == null)
+			{
+				throw new ArgumentNullException("methodnName");
+			}
+			try
+			{
+				return type.GetMethod(methodnName) != null;
+			}
+			catch (ArgumentNullException)
+			{
+				throw;
+			}
+		}
+	}
+}
