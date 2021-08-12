04906a12ddc9a189d316e3db1a16a8f87f7287f7
Connor Shearer <connor_shearer@outlook.com>
2021-08-12 07:51:41 +0100

    Make the client Program inline

--- Core/UEESA.Client/Program.cs
+++ Core/UEESA.Client/Program.cs
@@ -1,48 +1,38 @@
 using System;
 using System.Net.WebSockets;
-using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 
+using UEESA.Client;
 using UEESA.Client.Sockets;
 using UEESA.Client.Sockets.Handlers;
 using UEESA.Client.Data;
 using UEESA.Client.Data.States;
 using UEESA.Shared.Sockets;
 
-namespace UEESA.Client
-{
-    public static class Program
-    {
-        public static WebAssemblyHost Host { get; private set; }
+WebAssemblyHost Host;
 
-        public static async Task Main(string[] args)
-        {
-            WebAssemblyHostBuilder HostBuilder = WebAssemblyHostBuilder.CreateDefault(args);
-            HostBuilder.RootComponents.Add<App>("#app");
-            HostBuilder.Services.AddTransient<ConnectionManager<ClientWebSocket>>();
-            HostBuilder.Services.AddScoped<StateSocketHandler>();
-            HostBuilder.Services.AddScoped<ClientState>();
-            HostBuilder.Services.AddScoped<InitializationState>();
-            HostBuilder.Services.AddScoped<ServerState>();
-            HostBuilder.Services.AddScoped<LocalStorageState>();
-            HostBuilder.Services.AddScoped<WebSocketManagerMiddleware>();
+WebAssemblyHostBuilder HostBuilder = WebAssemblyHostBuilder.CreateDefault(args);
+HostBuilder.RootComponents.Add<App>("#app");
+HostBuilder.Services.AddTransient<ConnectionManager<ClientWebSocket>>();
+HostBuilder.Services.AddScoped<StateSocketHandler>();
+HostBuilder.Services.AddScoped<ClientState>();
+HostBuilder.Services.AddScoped<InitializationState>();
+HostBuilder.Services.AddScoped<ServerState>();
+HostBuilder.Services.AddScoped<LocalStorageState>();
+HostBuilder.Services.AddScoped<WebSocketManagerMiddleware>();
 
-            HostBuilder.Services.AddScoped<UIState>();
-            HostBuilder.Services.AddScoped<UIState.PageState>();
-            HostBuilder.Services.AddScoped<UIState.ComponentState>();
+HostBuilder.Services.AddScoped<UIState>();
+HostBuilder.Services.AddScoped<UIState.PageState>();
+HostBuilder.Services.AddScoped<UIState.ComponentState>();
 
-            HostBuilder.Services.AddScoped<JSInterface>();
-            HostBuilder.Services.AddScoped<JSInterface.Utilities>();
-            HostBuilder.Services.AddScoped<JSInterface.Runtime>();
-            HostBuilder.Services.AddScoped<JSInterface.Cache>();
-            HostBuilder.Services.AddScoped<JSInterface.LocalData>();
-            HostBuilder.Services.AddScoped<JSInterface.AnimationManager>();
+HostBuilder.Services.AddScoped<JSInterface>();
+HostBuilder.Services.AddScoped<JSInterface.Utilities>();
+HostBuilder.Services.AddScoped<JSInterface.Runtime>();
+HostBuilder.Services.AddScoped<JSInterface.Cache>();
+HostBuilder.Services.AddScoped<JSInterface.LocalData>();
+HostBuilder.Services.AddScoped<JSInterface.AnimationManager>();
 
-            Host = HostBuilder.Build();
-            Services.SetServiceProvider(Host.Services);
-            await Host.RunAsync();
-        }
-    }
-}
+Host = HostBuilder.Build();
+Services.SetServiceProvider(Host.Services);
+await Host.RunAsync();
