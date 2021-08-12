9fdb2cb17c99162b65d664f8c37a6bcccf72f809
Connor Shearer <connor_shearer@outlook.com>
2021-08-12 08:31:31 +0100

    Create Runtime.cs

---
+++ Core/UEESA.Shared/Runtime.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+public static class Runtime
+{
+	public static bool IsMono => RuntimeInformation.FrameworkDescription.Contains("Mono");
+
+	public static bool IsWASM => RuntimeInformation.OSDescription.Contains("Browser");
+
+	static Runtime()
+	{
+	}
+}
