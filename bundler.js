d009fe092cd3a5515e212c6c90b3a4f4800a0a6f
Connor Shearer <connor_shearer@outlook.com>
2021-08-29 04:11:57 +0100

    Add in support for woff2 and ttf fonts in the bundler and create the needed directories

--- bundler.js
+++ bundler.js
@@ -170,13 +170,15 @@ async function processing()
     console.log(' \\')
 
     const files = findFiles(__client_wwwrootdev_dirname)
-    const pngFiles = filterFiles(files, 'png')
-    const mp4Files = filterFiles(files, 'mp4')
-    const scssFile = __client_wwwrootdev_dirname + sep + 'css' + sep + 'bundle.scss'
     const jsFiles = filterFiles(files, 'js')
+    const scssFile = __client_wwwrootdev_dirname + sep + 'css' + sep + 'bundle.scss'
     const htmlFiles = filterFiles(files, 'html')
     const svgFiles = filterFiles(files, 'svg')
     const jsonFiles = filterFiles(files, 'json')
+    const woff2Files = filterFiles(files, 'woff2')
+    const ttfFiles = filterFiles(files, 'ttf')
+    const pngFiles = filterFiles(files, 'png')
+    const mp4Files = filterFiles(files, 'mp4')
 
     await processJS(jsFiles)
     processSASS(scssFile)
@@ -187,6 +189,7 @@ async function processing()
         {
             const output = item.path.replace('wwwroot-dev', 'wwwroot')
             console.log('  | Copying HTML: ' + item.path.replace(__client_wwwrootdev_dirname, '') + ' > ' + output.replace(__client_wwwroot_dirname, ''))
+            mkdirSync(dirname(output), { recursive: true })
             copyFileSync(item.path, output)
         }
     })
@@ -197,6 +200,7 @@ async function processing()
         {
             const output = item.path.replace('wwwroot-dev', 'wwwroot')
             console.log('  | Copying SVG: ' + item.path.replace(__client_wwwrootdev_dirname, '') + ' > ' + output.replace(__client_wwwroot_dirname, ''))
+            mkdirSync(dirname(output), { recursive: true })
             copyFileSync(item.path, output)
         }
     })
@@ -207,6 +211,29 @@ async function processing()
         {
             const output = item.path.replace('wwwroot-dev', 'wwwroot')
             console.log('  | Copying JSON: ' + item.path.replace(__client_wwwrootdev_dirname, '') + ' > ' + output.replace(__client_wwwroot_dirname, ''))
+            mkdirSync(dirname(output), { recursive: true })
+            copyFileSync(item.path, output)
+        }
+    })
+
+    woff2Files.forEach(item =>
+    {
+        if (needsCaching(item.path, 'woff2'))
+        {
+            const output = item.path.replace('wwwroot-dev', 'wwwroot')
+            console.log('  | Copying Font: ' + item.path.replace(__client_wwwrootdev_dirname, '') + ' > ' + output.replace(__client_wwwroot_dirname, ''))
+            mkdirSync(dirname(output), { recursive: true })
+            copyFileSync(item.path, output)
+        }
+    })
+
+    ttfFiles.forEach(item =>
+    {
+        if (needsCaching(item.path, 'ttf'))
+        {
+            const output = item.path.replace('wwwroot-dev', 'wwwroot')
+            console.log('  | Copying Font: ' + item.path.replace(__client_wwwrootdev_dirname, '') + ' > ' + output.replace(__client_wwwroot_dirname, ''))
+            mkdirSync(dirname(output), { recursive: true })
             copyFileSync(item.path, output)
         }
     })
@@ -217,6 +244,7 @@ async function processing()
         {
             const output = item.path.replace('wwwroot-dev', 'wwwroot').replace('.png', '.avif')
             console.log('  | Transcoding Image: ' + item.path.replace(__client_wwwrootdev_dirname, '') + ' > ' + output.replace(__client_wwwroot_dirname, ''))
+            mkdirSync(dirname(output), { recursive: true })
             if (fileExists(item.path.replace('wwwroot-dev', 'wwwroot')))
             {
                 unlinkSync(item.path.replace('wwwroot-dev', 'wwwroot'))
@@ -228,7 +256,7 @@ async function processing()
                 [
                     imageminAvif({ quality: 40, speed: 0 })
                 ]
-                })
+            })
             renameSync(output.replace('.avif', '.png'), output)
         }
     })
